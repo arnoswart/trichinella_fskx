664ed6ffb08aaa3b41b5ad0734f423c65a6cf524
Frau Dr. Petra Ganas <ganasp@it.bfr-science.de>
2021-11-10 15:55:11 +0100

    new files of fskx model trichinella-consumption

--- 2. consumption/simulations/defaultSimulation.r
+++ 2. consumption/simulations/defaultSimulation.r
@@ -1,8 +1,8 @@
-
-n_portions_per_part <- tibble(  diaphragm=2,
-                                shoulder =35,
-                                belly    =54, 
-                                loin     =144, 
-                                ham      =121, 
-                                other    =48)
+df_larvae_in_parts_join <- " "
 p_welldone <- 0.9
+diaphragm <- 2
+shoulder <- 35
+belly <- 54
+loin <- 144
+ham <- 121
+other <- 48
