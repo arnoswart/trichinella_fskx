411e68d9eac21346d5a36e23ac105e9c20061556
swarta <arno.swart@rivm.nl>
2022-02-16 11:34:42 +0100

    Default inactivation function provided -> no inactivation

--- 4. cooking model/simulations/defaultSimulation.R
+++ 4. cooking model/simulations/defaultSimulation.R
@@ -1,5 +1,7 @@
 df_larvae_in_portions_afterConsump_join <- " "
 df_zero_larvae_in_portions_afterConsump_join <- " "
+inactivation <- 'inactivation_func <- function(  I0, T0,T1, t1, inactivation_params ){return( I0 ) }'
+inactivation_params <- NULL
 rare_start_temp <- 20
 rare_end_temp <- 54
 rare_end_time <- 2.5
