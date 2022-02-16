f7d24f5e51511efdd4455db0f166a0ebed1fdc99
swarta <arno.swart@rivm.nl>
2022-02-16 10:37:56 +0100

    Greatly simplified table output for meat production

--- 1. meatproduction/visualization.r
+++ 1. meatproduction/visualization.r
@@ -1,26 +1,31 @@
 library( patchwork )
-library( flextable )
-library( ggplotify )
-library( magick )
 library(ggplot2)
-library(webshot)
+library(gridExtra)
+library(grid)
 
-wrap_plots(
-  swine_table %>% 
-    select( - simulation ) %>%
-    mutate( has_falseneg = (n.falseneg!=0) ) %>% 
-    group_by( has_falseneg ) %>% 
-    summarize( across( everything(), median, na.rm=T) ) %>% 
-    flextable() %>% 
-    add_header_row(values = c("False negatives  present",
-      "Number of swine",
-      "Number swine in false negative batches",
-      "Probability of swine from a false negative batch",
-      "Median number of swine  with zero larvae in a false negative batch",
-      "Median number of swine  with larvae  in a false negative batch",
-      "Probabiltiy of a positive swine in a false negative batch")) %>% 
-    as_raster() %>% 
-    as.ggplot(),
+plot_table <- swine_table %>% 
+  select( - simulation ) %>%
+  mutate( has_falseneg = (n.falseneg!=0) ) %>% 
+  group_by( has_falseneg ) %>% 
+  summarize( across( everything(), median, na.rm=T) ) 
+
+colnames( plot_table ) <-c("False negatives present",
+                           "Number of swine",
+                           "Number swine in \nfalse negative batches",
+                           "Probability of swine from\n a false negative batch",
+                           "Median number of swine  with\n zero larvae in a false negative batch",
+                           "Median number of swine  with\n larvae  in a false negative batch",
+                           "Probabiltiy of a positive swine in\n a false negative batch")
+
+plot_table <- plot_table %>% 
+  pivot_longer( -contains("False negatives present") ) %>% 
+  mutate( `False negatives present` = ifelse( `False negatives present`==TRUE,
+                                              "False negatives present",
+                                              "No false negatives present")) %>% 
+  pivot_wider( names_from=contains("False negatives present") )
+
+grid.arrange(
+  plot_table %>% tableGrob( rows=NULL ),
   df_larvae_in_parts %>% 
       pivot_longer(c(- simulation,-carcass), names_to="muscle", values_to="value") %>% 
       group_by( carcass, simulation ) %>% 
